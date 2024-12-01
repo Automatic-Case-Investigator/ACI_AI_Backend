c5e6ec683f53fc6388119237ea9d43c4061656f7
acezxn <acezxn@gmail.com>
2024-12-01 11:19:52 +0800

    Implemented load baseline model functionality and fixed multiple bugs

--- TaskGenTrainer_Endpoint/views.py
+++ TaskGenTrainer_Endpoint/views.py
@@ -6,53 +6,6 @@ from json import JSONDecodeError
 import json
 import uuid
 
-def add_case_data(request):
-    """Stores case data temporarily in redis
-
-    POST JSON body format:
-        ```json
-        {
-            "title": "...",
-            "description": "...",
-            "tasks" : [
-                {
-                    "title": "...",
-                    "description: "..."
-                },
-                ...
-            ]
-        }
-        ```
-        
-
-    Args:
-        request (_type_): django request
-
-    Returns:
-        JsonResponse: Json response
-    """
-    if request.method == "POST":
-        try:
-            request_data = json.loads(request.body)
-            formatted_data = dict()
-            formatted_data["title"] = request_data["title"]
-            formatted_data["description"] = request_data["description"]
-            formatted_data["tasks"] = []
-            for task in request_data["tasks"]:
-                formatted_data["tasks"].append({
-                    "title": task["title"],
-                    "description": task["description"]
-                })
-            
-            id = f"Case:{str(uuid.uuid4())}"
-            redis_client.set(id, json.dumps(formatted_data), ex=settings.REDIS_KEY_EXPIRY_TIME)
-            return JsonResponse({"message": "Success", "id": id})
-        except (KeyError, JSONDecodeError):
-            return JsonResponse({"error": "Data not formatted properly"})
-    
-    else:
-        return JsonResponse({"error": "Invalid method"})
-    
 def set_case_data(request):
     """Modifies the already stored case data in redis
 
